816ea118dc7cf55642c43cc8a1410d282e2537aa
Johannaespitia <johannaespitia91@gmail.com>
2022-11-29 17:49:53 -0500

    3preguntas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "hola";
 const nuevoNum = 34;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 01;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
